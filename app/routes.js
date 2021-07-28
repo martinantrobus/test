e42c7c2e711b3ee1f9c60da2eda9aee9c3a4acc7
Martin Antrobus <87323164+martinantrobus@users.noreply.github.com>
2021-07-28 11:39:30 +0100

    Routing added

--- app/routes.js
+++ app/routes.js
@@ -7,15 +7,28 @@ const router = express.Router()
 router.post('/juggling-balls-answer', function (req, res) {
 
   // Make a variable and give it the value from 'juggling-balls'
-  var jugglingBalls = req.session.data['juggling-balls']
+  var jugglingBalls = req.session.data['how-many-balls']
 
   // Check whether the variable matches a condition
-  if (jugglingBalls == "3 or more"){
+  if (jugglingBalls == '3 or more'){
     // Send user to next page
-    res.redirect('/juggling-trick')
+    res.redirect('juggling-trick')
   } else {
     // Send user to ineligible page
-    res.redirect('/ineligible')
+    res.redirect('ineligible')
+  }
+
+})
+
+// Run this code when a form is submitted to 'full-name-answer'
+router.post('/full-name-answer', function (req, res) {
+  var fullName = req.session.data['full-name']
+  if (fullName == 'Sam'){
+    // Send user to next page
+    res.redirect('ineligible')
+  } else {
+    // Send user to ineligible page
+    res.redirect('juggling-balls')
   }
 
 })
